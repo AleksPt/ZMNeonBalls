7cef3c236ad0b6f0d6b3951668310cddc66e000f
Alex Petrenko <alex.pt@ya.ru>
2025-01-27 14:35:01 +0300

    Initial Commit

---
+++ ZMNeonBalls/ZMNeonBallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMNeonBallsApp.swift
+//  ZMNeonBalls
+//
+//  Created by Алексей on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZMNeonBallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
